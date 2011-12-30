053f9077945215ceb5931cb2a5d85355b4855e1b
kimozor <kzfr.kimo@gmail.com>
2011-12-30 19:43:10 +0100

    build.sh modified -> tinyxml added

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 mkdir -p bin
 echo "G. Albespy, G. Breda, T. Gay, A. Mahé"
 echo "Compiling our Portal version : alidade, this will take some time"
-g++ src/*.cpp stein/*.cpp stein/glew/*.c -I. -lGL -lSDL -O3 -Wall -o bin/alidade
+g++ src/*.cpp stein/*.cpp stein/glew/*.c tinyxml/*.cpp -I. -lGL -lSDL -O3 -Wall -o bin/alidade
 echo "done ! Now go in the bin directory and launch ./alidade"
